0be393b94a1e15fdbdc320a8a4963f4aadc61059
Sachin Kumar <sachinkumar.gheura@gmail.com>
2026-01-02 16:31:23 +0530

    Update CORS configuration to change localhost port from 8000 to 8080

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ CORS(app, resources={
             "http://127.0.0.1:5500",
             "http://localhost:3000",
             "https://nicost-frontend.vercel.app",
-            "http://localhost:8000",
+            "http://localhost:8080",
             "http://127.0.0.1:8000"
         ],
         "methods": ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
